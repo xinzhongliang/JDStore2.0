238fe6a26afe33d27f2b69fd7c8e63e2be5d7c57
Johnson Liang <johnson.liangxinzhong@gmail.com>
2017-02-22 21:48:40 +0800

    add apply_cancel function

--- config/routes.rb
+++ config/routes.rb
@@ -22,10 +22,15 @@ Rails.application.routes.draw do
     member do
       post :pay_with_alipay
       post :pay_with_wechat
-      post :cancel
-      post :ship
-      post :shipped
-      post :return
+      post :apply_to_cancel
+    end
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
     end
   end
   namespace :account do
