40a731d4894d75c973a061eb7edfcb97617e7a48
Johnson Liang <johnson.liangxinzhong@gmail.com>
2017-02-23 17:18:51 +0800

    sendcloud settings

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,17 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.fog_provider = 'fog'
+
     config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
 
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
 
-      region:                ENV['AWS_REGION']
+      region:                ENV['ap-northeast-1']
 
     }
-    config.fog_directory = ENV['AWS_BUCKET_NAME']
+    config.fog_provider = 'fog'
+    config.fog_directory = ENV['fullstack-liangxinzhong']
 
   else
     config.storage :file
